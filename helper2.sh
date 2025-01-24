569c6be23d7226ed4f6c9d3baf76225ff999552d
Shane <develop@shenyingnan.com>
2025-01-24 16:56:48 +0800

    update script

--- helper2.sh
+++ helper2.sh
@@ -6,3 +6,20 @@ containers=$(docker ps --filter "label=helper.docker.lego.enable=true" --filter 
 
 matched_count=0
 successed_count=0
+
+for container in $containers; do
+    email=$(docker inspect -f '{{index .Config.Labels "helper.docker.lego.email"}}' "$container")
+    if [[ -n "$email" ]] && [[ "$email" != "$LEGO_ACCOUNT_EMAIL" ]]; then
+        continue
+    fi
+
+    ((matched_containers++))
+
+    echo "======"
+
+    #TODO FINISH IT
+
+    echo "======"
+done
+
+echo "Successed $successed_count/$matched_count"
