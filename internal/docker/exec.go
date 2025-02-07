4203f1682e190b8c0c1512980da9c06276180e52
Shane <develop@shenyingnan.com>
2025-02-07 13:51:11 +0800

    Create exec.go

---
+++ internal/docker/exec.go
@@ -0,0 +1,48 @@
+package docker
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+func exec(ctx context.Context, cli *client.Client, containerID string, cmd []string) (stdout, stderr string, err error) {
+	config := container.ExecOptions{
+		Cmd:          cmd,
+		AttachStdout: true,
+		AttachStderr: true,
+	}
+
+	id, err := cli.ContainerExecCreate(ctx, containerID, config)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to create exec: %v", err)
+	}
+
+	response, err := cli.ContainerExecAttach(ctx, id.ID, container.ExecAttachOptions{})
+	if err != nil {
+		return "", "", fmt.Errorf("failed to attach to exec: %v", err)
+	}
+	defer response.Close()
+
+	var stdoutBuffer, stderrBuffer bytes.Buffer
+	_, err = stdcopy.StdCopy(&stdoutBuffer, &stderrBuffer, response.Reader)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to read exec output: %v", err)
+	}
+
+	inspectResponse, err := cli.ContainerExecInspect(ctx, id.ID)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to inspect exec: %v", err)
+	}
+
+	if inspectResponse.ExitCode != 0 {
+		err = fmt.Errorf("command exited with code %d", inspectResponse.ExitCode)
+	}
+	stdout = stdoutBuffer.String()
+	stderr = stderrBuffer.String()
+	return
+}
